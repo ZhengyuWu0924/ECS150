8c30fd531b912116fa66f79635e3ece44748b70d
ZhengyuWu0924 <zywwu@ucdavis.edu>
2020-10-13 23:10:31 -0700

    features: implement system() function
    add new function sshellSystem(), implemented by using fork+exec+wait method.
    use execvp() as required to automatically search programs in $PATH
    
    Warning: this function is basically copied and modified from professor's lecture code, need to remove some parts later.

--- sshell.c
+++ sshell.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <unistd.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define CMDLINE_MAX 512
+
+// Implement the system() function by using fork+exec+wait method
+int sshellSystem(char *cmdString){
+    pid_t pid;
+    char *args[] = {"sh","-c",cmdString, NULL};
+    int ret;
+    pid = fork();
+    if(pid == 0){
+        execvp(args[0], args);
+        perror("execvp");
+        exit(1);
+    }else if(pid > 0){
+        int status;
+        waitpid(pid, &status, 0);
+        // printf("completed %d\n ", WEXITSTATUS(status));
+    }else{
+        perror("fork");
+        exit(1);
+    }
+    return 0;
+}
 
 int main(void)
 {
@@ -41,8 +64,9 @@ int main(void)
                 }
 
                 /* Regular command */
-                retval = system(cmd);
-                fprintf(stdout, "Return status value for '%s': %d\n",
+                // retval = system(cmd);
+                retval = sshellSystem(cmd);
+                fprintf(stdout, "+ completed '%s': [%d]\n",
                         cmd, retval);
         }
 
