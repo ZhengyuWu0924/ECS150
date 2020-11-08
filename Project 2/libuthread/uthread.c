826e8251697ddd8f808c7fddcba6fb9e0c083c95
akashmalode22 <45553815+akashmalode22@users.noreply.github.com>
2020-11-07 23:22:15 -0800

    Add files via upload
    Added basic logic of uthread_start and uthread_create. Not tested.

--- Project 2/libuthread/uthread.c
+++ Project 2/libuthread/uthread.c
@@ -8,9 +8,14 @@
 
 #include "private.h"
 #include "uthread.h"
+#include "queue.h"
+
+queue_t threads;
 
 struct uthread_tcb {
-	/* TODO Phase 2 */
+	int state; // 0 for running, 1 for ready, 2 for exited
+    uthread_ctx_t ctx; // context for the thread. We will set context during thread creation
+    char *stack; // We will initialize this during thread creation
 };
 
 struct uthread_tcb *uthread_current(void)
@@ -30,12 +35,43 @@ void uthread_exit(void)
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+    // First, create a TCB for thread. Set state to ready, initialize stack, initialize context
+    uthread_tcb thread = malloc(sizeof(uthread_tcb));
+
+    if (thread == NULL) {
+        return -1;
+    }
+
+    thread->state = 1;
+    thread->stack = (char*)uthread_ctx_alloc_stack();
+    //
+    // CHECK void *top_of_stack IS SET TO 1. CHECK IF CORRECT.
+    //
+	if (uthread_ctx_init(thread->ctx, 1, func, arg) == -1) { // type conversion because function returns void*
+        return -1;
+    }
+    // Now that the thread is set up and is ready to run, we can push it into the queue (phase 1)
+    queue_enqueue(threads, thread);
+    return 0;
 }
 
 int uthread_start(uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+    // queue_t threads was declared as a global variable. Initialize it here
+    threads = queue_create();
+
+	// Create a new thread
+    // Infinite while loop until no more threads are ready to run
+
+    if (uthread_create(func, arg) == -1) {
+        return -1;
+    }
+
+    // Infinite loop until no more threads are ready to run in the system
+    while (queue_length(threads) != 0) {
+        // Keep going until there are no more ready threads
+    }
+
 }
 
 void uthread_block(void)
@@ -47,4 +83,3 @@ void uthread_unblock(struct uthread_tcb *uthread)
 {
 	/* TODO Phase 2/3 */
 }
-
